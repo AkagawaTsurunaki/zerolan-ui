f0d864f4fb2e44663238e8df5d180a3d5dc20a6b
AkagawaTsurunaki <AkagawaTsurunaki@outlook.com>
2024-11-11 22:03:10 +0800

    test: Test for info toast passed

--- tests/test_toast.py
+++ tests/test_toast.py
@@ -1,13 +1,5 @@
-from multiprocessing import Process
-import time
-
 from zerolan.ui.api.toasts import Toast
-from zerolan.ui.app import start_ui_application
+
 
 def test_info_toast():
-    # p = Process(target=start_ui_application)
-    # p.start()
-    time.sleep(5)
     Toast("Test for showing toast!", "info", duration=5).show_toast()
-    time.sleep(5)
-    # p.kill()
